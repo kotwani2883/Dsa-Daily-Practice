08022091e3087b237a7c9af9deba55d5010082df
Palak <palakkotwani2883@gmail.com>
2021-01-14 17:36:48 +0530

    String Folder Added

---
+++ String/ReverseString.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=s.size()/2;
+       for(int i=0;i<l;i++)
+       {
+           swap(s[i],s[s.size()-i-1]);
+       }
+    }
+};
