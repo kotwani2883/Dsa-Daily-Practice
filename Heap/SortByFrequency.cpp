83476022fa7660aa6e7b79ec5d4f7369140a45a4
Palak <palakkotwani2883@gmail.com>
2021-02-26 14:58:07 +0530

    Heap Problem 4 added!

---
+++ Heap/SortByFrequency.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char,int>mp;
+        for(int i=0;i<s.length();i++)
+            mp[s[i]]++;
+        //create a max -heap
+        priority_queue<pair<int,char>>maxh;
+        for(auto it:mp)
+            maxh.push({it.second,it.first});
+        string res;
+        while(maxh.empty()==false)
+        {
+            int d=maxh.top().first;
+            while(d --)
+            {
+                res.push_back(maxh.top().second);
+               
+            }
+            maxh.pop();
+        }
+        return res;
+    }
+};/*Author-:Palak*/
+/*
+ * Only write code when you are sure of the approach .Remember of penalties!!
+ * Do optimized thinking (Think which technique/observation can be used,deep drive into the problem!!).
+ * Do post analysis after the contest./Upsolving is much more important than giving contest
+ * Learn a technique and solve some problems if its new to you--->Solve as many as problems to be confident in those kinds of problem!!
+ * DON'T GET STUCK ON ONE APPROACH,Move your mind in all directions
+ *Remember who you are and your Capabilities!!
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+bool isPrime(ll n)
+{
+	//Corner case:
+	if(n<=1)
+	return false;
+	if(n<=3)
+	return true;
+
+	//This optimizes the complexity:
+	if(n%2==0||n%3==0)
+	return false;
+	for(ll i=5;i*i<=n;i=i+6)
+	{
+		if(n%i==0||n%(i+2)==0)
+		return false;
+	}
+	return true;
+}
+int main()
+{
+	ll t;
+	cin>>t;
+
